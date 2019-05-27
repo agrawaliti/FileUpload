1feb983330e40f877f342e28e4324e4f085d2d3b
agrawaliti <agrawaliti0107@gmail.com>
2019-05-27 10:26:08 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,8 @@
+var initialState = {
+    state:kIMNativeStateInit,
+    placementId: 756,
+    adsConfig["xmlConfigUrl"] = " https://i.l.inmobicdn.cn/sdk/sdk/MMA/im_sdkconfig.xml";
+}
+
+console.log("Lets start loading");
+console.log("Iti Agrawal Loaded calc.js.");
